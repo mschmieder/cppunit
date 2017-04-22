71e32c1130b6fcb9dbff8c958357f814221d0abf
Matthias Schmieder <schmieder.matthias@gmail.com>
2017-04-22 23:16:04 +0200

    added CMake build system support for cppunit

---
+++ config.h.in.cmake
@@ -0,0 +1,106 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if library uses std::string::compare(string,pos,n) */
+#cmakedefine CPPUNIT_FUNC_STRING_COMPARE_STRING_FIRST 1
+
+/* define if the library defines strstream */
+#cmakedefine CPPUNIT_HAVE_CLASS_STRSTREAM 1
+
+/* Define to 1 if you have the <cmath> header file. */
+#cmakedefine CPPUNIT_HAVE_CMATH 1
+
+/* define if the compiler supports basic C++11 syntax */
+#cmakedefine CPPUNIT_HAVE_CXX11 1
+
+/* Define if you have the GNU dld library. */
+#cmakedefine CPPUNIT_HAVE_DLD 1
+
+/* Define to 1 if you have the `dlerror' function. */
+#cmakedefine CPPUNIT_HAVE_DLERROR 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine CPPUNIT_HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `finite' function. */
+#cmakedefine CPPUNIT_HAVE_FINITE 1
+
+/* define if the compiler supports GCC C++ ABI name demangling */
+#cmakedefine CPPUNIT_HAVE_GCC_ABI_DEMANGLE 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine CPPUNIT_HAVE_INTTYPES_H 1
+
+/* define if compiler has isfinite */
+#cmakedefine CPPUNIT_HAVE_ISFINITE 1
+
+/* Define if you have the libdl library or equivalent. */
+#cmakedefine CPPUNIT_HAVE_LIBDL 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine CPPUNIT_HAVE_MEMORY_H 1
+
+/* define if the compiler implements namespaces */
+#cmakedefine CPPUNIT_HAVE_NAMESPACES 1
+
+/* Define if you have the shl_load function. */
+#cmakedefine CPPUNIT_HAVE_SHL_LOAD 1
+
+/* define if the compiler has stringstream */
+#cmakedefine CPPUNIT_HAVE_SSTREAM 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine CPPUNIT_HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine CPPUNIT_HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine CPPUNIT_HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine CPPUNIT_HAVE_STRING_H 1
+
+/* Define to 1 if you have the <strstream> header file. */
+#cmakedefine CPPUNIT_HAVE_STRSTREAM 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine CPPUNIT_HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine CPPUNIT_HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine CPPUNIT_HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#cmakedefine CPPUNIT_LT_OBJDIR 1
+
+/* Disable debugging information */
+#cmakedefine CPPUNIT_NDEBUG 1
+
+/* Name of package */
+#cmakedefine CPPUNIT_PACKAGE 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine CPPUNIT_PACKAGE_BUGREPORT 1
+
+/* Define to the full name of this package. */
+#cmakedefine CPPUNIT_PACKAGE_NAME 1
+
+/* Define to the full name and version of this package. */
+#cmakedefine CPPUNIT_PACKAGE_STRING 1
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine CPPUNIT_PACKAGE_TARNAME 1
+
+/* Define to the home page for this package. */
+#cmakedefine CPPUNIT_PACKAGE_URL 1
+
+/* Define to the version of this package. */
+#cmakedefine CPPUNIT_PACKAGE_VERSION 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine CPPUNIT_STDC_HEADERS 1
+
+/* Version number of package */
+#cmakedefine CPPUNIT_VERSION 1
